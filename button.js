392372d7a8f876f8631acc5c403c53fdd83aa96f
Michal-Oslizlo17 <werminew@ogarnij.se>
2023-02-15 08:25:00 +0100

    HTML edit

--- button.js
+++ button.js
@@ -1,11 +1,3 @@
-document.getElementById("register-button").addEventListener("click", function () {
-  window.location.href = "rejestracja.html";
-});
-
-document.getElementById("database-button").addEventListener("click", function () {
-  window.location.href = "baza.php";
-});
-
 document.getElementById("return-button").addEventListener("click", function () {
   window.location.href = "main.html";
 });
